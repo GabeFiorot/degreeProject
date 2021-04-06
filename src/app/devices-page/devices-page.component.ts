74e431ba519b7d276326aeb6b3579336921534b4
SeanDunlop <30559729+SeanDunlop@users.noreply.github.com>
2021-04-05 23:11:38 -0400

    made shit poppin

--- src/app/devices-page/devices-page.component.ts
+++ src/app/devices-page/devices-page.component.ts
@@ -11,52 +11,25 @@ import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule, F
 export class DevicesPageComponent implements OnInit {
 
   constructor(private httpClient: HttpClient, private router:Router,private fb:FormBuilder) { }
+
+  position:number[] = [250,520,250,200,630,130];
   devices:Device[];
   SERVER_URL: string = 'https://luxo-api-test.azurewebsites.net/api/Devices/';
   SECURE_URL: string = 'https://luxo-api-test.azurewebsites.net/api/SecureDevices';
   loginForm:FormGroup;
-  LOGIN_URL: string  = 'https://luxo-api-test.azurewebsites.net/api/Login';
+  //LOGIN_URL: string  = 'https://luxo-api-test.azurewebsites.net/api/Login';
   token: string;
   ngOnInit(): void {
     this.devices = [];
     //this.getDevices();
+    this.token = history.state.data.token;
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+    this.getDevicesSecure();
   }
 
-  tryLogin()
-  {
-    var username = this.loginForm.get('username').value;
-    var password = this.loginForm.get('password').value;
-    var json;
-    var url = this.LOGIN_URL + '?username=' + username + '&hashpass=';
-    //console.log(this.sha256(password));
-    /*
-    this.httpClient
-      .get<any>(this.LOGIN_URL)
-      .subscribe(res => {
-        json = res;
-        console.log(json);
-        
-      } , err => console.log(err));
-      */
-      this.sha256(password).then(hash => {
-        //console.log(url + hash)
-        this.httpClient
-          .get<any>(url + hash)
-          .subscribe(res => {
-            json = res;
-            console.log("json" + json);
-        
-      } , err => {
-        console.log(err.error.text)
-        this.token = err.error.text;
-        this.getDevicesSecure();
-      });
-      });
-  }
 
   goToSchedules(): void {
     this.router.navigate(['/schedule'], {state: {data: {myData : 'example_data'}}});
@@ -65,7 +38,7 @@ export class DevicesPageComponent implements OnInit {
   editDevice(i:number)
   {
     var deviceId = this.devices[i].deviceId;
-    this.router.navigate(['/schedule'], {state: {data: {deviceIndex : deviceId}}});
+    this.router.navigate(['/schedule'], {state: {data: {deviceIndex : deviceId, token : this.token}}});
   }
 
   getDevicesSecure()
@@ -95,19 +68,5 @@ export class DevicesPageComponent implements OnInit {
       } , err => console.log(err));
   }
 
-   async sha256(message) {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder().encode(message);                    
-
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-
-    // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-    // convert bytes to hex string                  
-    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-    return hashHex;
-}
 
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Device} from '../Device';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule, FormArray, Form} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
   position:number[] = [250,520,250,200,630,130];
-  constructor() { }
-
+  constructor(private httpClient: HttpClient, private router:Router,private fb:FormBuilder) { }
+  SERVER_URL: string = 'https://luxo-api-test.azurewebsites.net/api/Devices/';
+  SECURE_URL: string = 'https://luxo-api-test.azurewebsites.net/api/SecureDevices';
+  loginForm:FormGroup;
+  LOGIN_URL: string  = 'https://luxo-api-test.azurewebsites.net/api/Login';
+  token: string;
   ngOnInit(): void {
+
+    //this.getDevices();
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  tryLogin()
+  {
+    var username = this.loginForm.get('username').value;
+    var password = this.loginForm.get('password').value;
+    var json;
+    var url = this.LOGIN_URL + '?username=' + username + '&hashpass=';
+    //console.log(this.sha256(password));
+    /*
+    this.httpClient
+      .get<any>(this.LOGIN_URL)
+      .subscribe(res => {
+        json = res;
+        console.log(json);
+        
+      } , err => console.log(err));
+      */
+      this.sha256(password).then(hash => {
+        //console.log(url + hash)
+        this.httpClient
+          .get<any>(url + hash)
+          .subscribe(res => {
+            json = res;
+            console.log("json" + json);
+        
+      } , err => {
+        console.log(err.error.text)
+        this.token = err.error.text;
+        this.router.navigate(['/devices'], {state: {data: {token : this.token}}});
+      });
+      });
+  }
+
+  async sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message);                    
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string                  
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    return hashHex;
+}
+  
 }
